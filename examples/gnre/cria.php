72796b07df44c9c0e44db35a5624137ce2951232
Gabriel Arantd Felipe <grantd.ctt@gmail.com>
2025-03-12 15:36:53 -0300

    Update

--- examples/gnre/cria.php
+++ examples/gnre/cria.php
@@ -11,14 +11,14 @@ use CloudDfe\SdkPHP\Gnre;
  */
 try {
 
-    // Variaveis para definição de configurações iniciais para o uso da SDK
+    // Variáveis para definição de configurações iniciais para o uso da SDK
     // Token: Token do emitente (distribuído pela CloudDFe se baseando no ambiente: homologação/produção)
     // Ambiente: Ambiente do qual o serviço vai ser executado (1- Produção / 2- Homologação)
     // Options: Opções para configuração da chamada da SDK
-    // Debug: Habilita ou desabilita mensagens de debug (Por enquando sem efeito)
+    // Debug: Habilita ou desabilita mensagens de debug (Por enquanto sem efeito)
     // Timeout: Tempo de espera para a execução da chamada
     // Port: Porta de comunicação
-    // Http_version: Versão do HTTP (Especifico para a comunicação utilizando PHP)
+    // Http_version: Versão do HTTP (Específico para a comunicação utilizando PHP)
 
     $params = [
         "token" => "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbXAiOiJ0b2tlbl9leGVtcGxvIiwidXNyIjoidGsiLCJ0cCI6InRrIn0.Tva_viCMCeG3nkRYmi_RcJ6BtSzui60kdzIsuq5X-sQ",
@@ -34,8 +34,7 @@ try {
     $gnre = new Gnre($params);
 
     // Payload: Informações que serão enviadas para a API da CloudDFe
-
-    // OBS: Não utilize o payload de exemplo abaixo, ele é apenas um exemplo. Consulte a documentação para construir o payload para sua aplicação.
+    // OBS: NÃO UTILIZE O PAYLOAD DE EXEMPLO, ELE É APENAS UM EXEMPLO. CONSULTE A DOCUMENTAÇÃO PARA CONSTRUIR O PAYLOAD PARA SUA APLICAÇÃO.
 
     $payload = [
         "numero" => "6",
@@ -87,61 +86,85 @@ try {
         ]
     ];
 
+    // Enviar a GNRe para API
     $resp = $gnre->cria($payload);
     echo "<pre>";
     print_r($resp);
     echo "</pre>";
+
     if ($resp->sucesso) {
+        // Essa condição verifica se a GNRe foi enviada para processamento
+        // Altere o status da GNRe para (Em processamento)
+
         $chave = $resp->chave;
-        sleep(15);
+        sleep(15); // O Ideal é aguardar de 10 a 15 segundos para consultar a GNRe ou utilizar o WEBHOOK
+        
         $payload = [
             "chave" => $chave
         ];
-        $resp = $cteos->consulta($payload);
+    
+        // Consulta a GNRe
+        $resp = $gnre->consulta($payload);
+        
+        // Verifica se a GNRe não está em processamento
         if ($resp->codigo != 5023) {
             if ($resp->sucesso) {
-                // autorizado
+                // se a GNRe foi autorizada ela será retornada aqui
+                // salvar as informações da GNRe em seu banco de dados e alterar o status para (Autorizado)
                 var_dump($resp);
             } else {
-                // rejeição
+                // se a GNRe foi rejeitada ela será retornada aqui
+                // salvar as informações de erro da GNRe em seu banco de dados e alterar o status para (Rejeitado)
                 var_dump($resp);
             }
         } else {
-            // nota em processamento
-            // recomendamos que seja utilizado o metodo de consulta manual ou o webhook
+            // se a GNRe estiver em processamento ela será retornada aqui
+            // salvar as informações da GNRe em seu banco de dados e alterar o status para (Em processamento)
+            // RECOMENDAMOS UTILIZAR O WEBHOOK POIS EVITA DE SEU CLIENTE FICAR FAZENDO CONSULTAS
+            var_dump($resp);
         }
+
     } else if (in_array($resp->codigo, [5001, 5002])) {
-        // erro nos campos
+        // se a GNRe estiver com dados faltando ou dando erro ao gerar o XML ela será retornada aqui
+        // salvar o status da GNRe como (Rejeitado/Erro) OBS: Não foi a SEFAZ que rejeitou mas sim nossa API que existe campos obrigatórios que não foram preenchidos.
+        // Salvar as informações de erro da GNRe e apresentar ao usuário
         var_dump($resp->erros);
     } else if ($resp->codigo == 5008) {
+        // se a GNRe já foi criada ela será retornada aqui
+        // ATENÇÃO: SE UTILIZADO INCORRETAMENTE PODE SOBREESCREVER DOCUMENTOS.
+        
+        // O procedimento obtém a chave da GNRe e consulta para verificar se a GNRe foi autorizada, rejeitada ou se ainda está em processamento
+        // porém se no seu sistema tiver salvando os status da GNRe incorretamente pode sobreescrever documentos 
+
         $chave = $resp->chave;
-        // >= 7000 erro de timout ou de conexão
-        // 5008 documento já criado
-        var_dump($resp);
         $payload = [
             "chave" => $chave
-        ];
-        // recomendamos fazer a consulta pela chave para sincronizar o documento
+        ]; 
+
+        // Vai realizar a consulta da GNRe para verificar o status da GNRe
         $resp = $gnre->consulta($payload);
         if ($resp->codigo != 5023) {
             if ($resp->sucesso) {
-                // autorizado
+                // se a GNRe foi autorizada ela será retornada aqui
+                // salvar as informações da GNRe em seu banco de dados e alterar o status para (Autorizado)
                 var_dump($resp);
             } else {
-                // rejeição
+                // se a GNRe foi rejeitada ela será retornada aqui
+                // salvar as informações de erro da GNRe em seu banco de dados e alterar o status para (Rejeitado)
                 var_dump($resp);
             }
         } else {
-            // em processamento
+            // se a GNRe estiver em processamento ela será retornada aqui
+            // salvar as informações da GNRe em seu banco de dados e alterar o status para (Em processamento)
+            // RECOMENDAMOS UTILIZAR O WEBHOOK POIS EVITA DE SEU CLIENTE FICAR FAZENDO CONSULTAS
             var_dump($resp);
         }
     } else {
-        // rejeição
+        // Se ocorrer qualquer outro erro será retornado aqui
+        // salvar as informações de erro da GNRe em seu banco de dados e alterar o status para (Rejeitado)
         var_dump($resp);
     }
 } catch (\Exception $e) {
-
     // Em caso de erros será lançado uma exceção com a mensagem de erro
-
     echo $e->getMessage();
 }
